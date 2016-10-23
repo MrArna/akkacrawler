2135e37108730fe0e9d57ead8ec6bde29f517c4a
Andrea Tirinzoni <andrea.tirinzoni@gmail.com>
2016-10-23 14:58:54 -0500

    Stubs of all actors have been added

---
+++ src/main/scala-2.11/edu/uic/cs474/hw3/analysis/ProjectAnalyzer.scala
@@ -0,0 +1,8 @@
+package edu.uic.cs474.hw3.analysis
+
+/**
+  * Created by andrea on 23/10/16.
+  */
+class ProjectAnalyzer {
+
+}
---
+++ src/main/scala-2.11/edu/uic/cs474/hw3/http/ProjectDownloader.scala
@@ -0,0 +1,8 @@
+package edu.uic.cs474.hw3.http
+
+/**
+  * Created by andrea on 23/10/16.
+  */
+class ProjectDownloader {
+
+}
