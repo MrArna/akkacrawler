a6586d7fca7e86827df8417705e6e4a5250d951c
Alessandro Pappalardo <alessandro1.pappalardo@mail.polimi.it>
2016-11-08 22:08:35 -0600

    [Versioning] Fix tags retrieval

--- src/main/scala-2.11/edu/uic/cs474/hw3/Main.scala
+++ src/main/scala-2.11/edu/uic/cs474/hw3/Main.scala
@@ -2,11 +2,11 @@ package edu.uic.cs474.hw3
 
 import akka.actor.{ActorSystem, Props}
 import com.scitools.understand.{Database, Understand}
-import edu.uic.cs474.hw3.analysis.NVersionsFirstLast
+import edu.uic.cs474.hw3.analysis.{NVersionsFirstLast, NVersionsTwoByTwo}
 import edu.uic.cs474.hw3.graphing.ReferenceGraphBuilder
 import edu.uic.cs474.hw3.messages.Start
 import edu.uic.cs474.hw3.undestand.DbManager
-import edu.uic.cs474.hw3.versioning.CommitPolicy
+import edu.uic.cs474.hw3.versioning.{CommitPolicy, TagPolicy}
 
 /**
   * The entry point of the application
@@ -16,8 +16,8 @@ object Main extends App {
   val master = system.actorOf(Props[Master])
   Config.maxProjectVersionManagers_=(3)
   Config.maxProjectVersionParsers_=(3)
-  Config.maxNVersions_=(20)
+  Config.maxNVersions_=(5)
   Config.analysisPolicy_=(NVersionsFirstLast)
-  Config.versionPolicy_=(CommitPolicy)
-  master ! Start(2, "tetris", "java")
+  Config.versionPolicy_=(TagPolicy)
+  master ! Start(2, "picasso", "java")
 }
