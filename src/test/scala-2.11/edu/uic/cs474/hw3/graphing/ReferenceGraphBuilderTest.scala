5c4705fd1d4a4757fff7a22463da18f5fe643fad
Alessandro Pappalardo <alessandro1.pappalardo@mail.polimi.it>
2016-11-09 11:08:02 -0600

    [Testing] Expand graph generation testing

--- src/test/scala-2.11/edu/uic/cs474/hw3/graphing/ReferenceGraphBuilderTest.scala
+++ src/test/scala-2.11/edu/uic/cs474/hw3/graphing/ReferenceGraphBuilderTest.scala
@@ -16,25 +16,36 @@ import org.scalatest.mockito.MockitoSugar
   */
 class ReferenceGraphBuilderTest extends FunSuite with MockitoSugar {
 
-  val mockDbManager = mock[DbManager]
-  val firstClass = mock[Entity]
-  val secondClass = mock[Entity]
-  val extendReference: Reference = mock[Reference]
-  val classKind = mock[Kind]
-  var classEntities = List[Entity](firstClass, secondClass)
-  val expectedGraph: DirectedGraph[EntityVertex, ReferenceEdge] = new SimpleDirectedGraph[EntityVertex, ReferenceEdge](new ClassBasedEdgeFactory[EntityVertex, ReferenceEdge](classOf[ReferenceEdge]))
-  val firstClassVertex: ClassVertex = new ClassVertex("package.class1", "long.package.class1")
-  val secondClassVertex: ClassVertex = new ClassVertex("package.class2", "long.package.class2")
-  
-  test("testBuildClassEntities") {
+  test("testBuildGraph") {
+
+    val mockDbManager = mock[DbManager]
+    val firstClass = mock[Entity]
+    val secondClass = mock[Entity]
+    val interface = mock[Entity]
+    val extendReference: Reference = mock[Reference]
+    val interfaceReference: Reference = mock[Reference]
+    val classKind = mock[Kind]
+    val interfaceKind = mock[Kind]
+    var classEntities = List[Entity](firstClass, secondClass)
+    val expectedGraph: DirectedGraph[EntityVertex, ReferenceEdge] = new SimpleDirectedGraph[EntityVertex, ReferenceEdge](new ClassBasedEdgeFactory[EntityVertex, ReferenceEdge](classOf[ReferenceEdge]))
+    val firstClassVertex: ClassVertex = new ClassVertex("package.class1", "long.package.class1")
+    val secondClassVertex: ClassVertex = new ClassVertex("package.class2", "long.package.class2")
+    val interfaceVertex: InterfaceVertex = new InterfaceVertex("package.interface", "long.package.interface")
 
     Mockito.when(classKind.name).thenReturn("Class")
+    Mockito.when(interfaceKind.name).thenReturn("Interface")
     Mockito.when(extendReference.ent).thenReturn(secondClass)
+    Mockito.when(interfaceReference.ent).thenReturn(interface)
+
+    Mockito.when(interface.simplename).thenReturn("package.interface")
+    Mockito.when(interface.longname(true)).thenReturn("long.package.interface")
+    Mockito.when(interface.kind).thenReturn(interfaceKind)
+    Mockito.when(firstClass.refs(Extend.kind, Interface.kind, true)).thenReturn(Array[Reference]())
 
     Mockito.when(firstClass.simplename).thenReturn("package.class1")
     Mockito.when(firstClass.longname(true)).thenReturn("long.package.class1")
     Mockito.when(firstClass.kind).thenReturn(classKind)
-    Mockito.when(firstClass.refs(Implement.kind, Interface.kind, true)).thenReturn(Array[Reference]())
+    Mockito.when(firstClass.refs(Implement.kind, Interface.kind, true)).thenReturn(Array(interfaceReference))
     Mockito.when(firstClass.refs(Extend.kind, Class.kind, true)).thenReturn(Array(extendReference))
     Mockito.when(firstClass.refs(Define.kind, FieldVariable.kind, true)).thenReturn(Array(extendReference))
     Mockito.when(firstClass.refs(Define.kind, Method.kind, true)).thenReturn(Array(extendReference))
@@ -44,8 +55,8 @@ class ReferenceGraphBuilderTest extends FunSuite with MockitoSugar {
     Mockito.when(secondClass.kind).thenReturn(classKind)
     Mockito.when(secondClass.refs(Implement.kind, Interface.kind, true)).thenReturn(Array[Reference]())
     Mockito.when(secondClass.refs(Extend.kind, Class.kind, true)).thenReturn(Array[Reference]())
-    Mockito.when(secondClass.refs(Define.kind, FieldVariable.kind, true)).thenReturn(Array(extendReference))
-    Mockito.when(secondClass.refs(Define.kind, Method.kind, true)).thenReturn(Array(extendReference))
+    Mockito.when(secondClass.refs(Define.kind, FieldVariable.kind, true)).thenReturn(Array[Reference]())
+    Mockito.when(secondClass.refs(Define.kind, Method.kind, true)).thenReturn(Array[Reference]())
 
     Mockito.when(mockDbManager.getEntityListByTypeListFromDb(Class)).thenReturn(classEntities)
 
@@ -55,9 +66,14 @@ class ReferenceGraphBuilderTest extends FunSuite with MockitoSugar {
 
     expectedGraph.addVertex(firstClassVertex)
     expectedGraph.addVertex(secondClassVertex)
+    expectedGraph.addVertex(interfaceVertex)
     expectedGraph.addEdge(firstClassVertex, secondClassVertex, new ClassExtendEdge(firstClassVertex, secondClassVertex))
+    expectedGraph.addEdge(firstClassVertex, interfaceVertex, new ImplementEdge(firstClassVertex, interfaceVertex))
 
     assert(expectedGraph.vertexSet() == graph.vertexSet())
+    assert(expectedGraph.edgeSet() == graph.edgeSet())
   }
+
+
 
 }
